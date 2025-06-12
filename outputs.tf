b4c5160b0849e273da7af83c581bef7ff41dcda3
cliber castillo <cliber.castillo@fibernaas.onmicrosoft.com>
2025-06-12 14:23:57 -0500

    change output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,4 @@
-output "disk_name" {
-  value       = azurerm_managed_disk.oym_disk_0.name
-  description = "The name of the managed disk."
-}
-output "vm_name" {
-  value       = data.azurerm_virtual_machine.oym_existing_vm.name
-  description = "The name of the existing virtual machine."
+output "logic_app_name" {
+  value       = azurerm_logic_app_workflow.example.name
+  description = "The name of the Logic App workflow."
 }
