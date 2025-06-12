57bbb32d6aab934801b65ec90d4e99e1921d0c71
cliber castillo <cliber.castillo@fibernaas.onmicrosoft.com>
2025-06-12 14:46:56 -0500

    output RSG

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "logic_app_name" {
   value       = azurerm_logic_app_workflow.example.name
   description = "The name of the Logic App workflow."
 }
+
+output "resource_group_name" {
+  value       = azurerm_resource_group.example.name
+  description = "The name of the resource group where the Logic App is deployed."
+}
